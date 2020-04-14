7de2e4cc5fe0cd3de211435129ad3c2f25917e01
Stephen Clements <mrevilg@gmail.com>
2020-04-14 06:54:31 -0500

    images

--- listing4-1.py
+++ listing4-1.py
@@ -10,3 +10,8 @@ PLAYER_NAME = "Stephen"
 FRIEND1_NAME = "Danny G"
 FRIEND2_NAME = "Luigi"
 current_room = 31 # start room = 31
+
+top_left_x = 100
+top_left_y = 150
+
+DEMO_OBJECTS = [images.floor, images.pillar, images.soil]
