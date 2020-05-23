617f75d734b994ff8ecc838e3b181158540e965c
Stephen Clements <mrevilg@gmail.com>
2020-05-23 13:11:12 -0500

    4-2 update

--- listing4-2.py
+++ listing4-2.py
@@ -79,3 +79,47 @@ def generate_map(): # This makes the map of the current room using room, scenery
     if current_room in range(1, 21):
         bottom_edge = 2 # soil
         side_edge = 2 # soil
+    if current_room in range(21, 26):
+        bottom_edge = 1 # wall
+        side_edge = 2 # soil
+    if current_room > 25:
+        bottom_edge = 1 # wall
+        side_edge = 1 # wall
+
+    # Create top line of room map
+    room_map=[[side_edge] * room_width]
+    # Add lines
+    for y in range(room_height - 2):
+        room_map.append([side_edge] + [floor_type]*(room_width - 2) + [side_edge])
+    # Add line to bottom
+    room_map.append([bottom_edge] * room_width)
+
+    # Add doorways
+    middle_row = int(room_height / 2)
+    middle_column = int(room_width / 2)
+
+    if room_data[4]: # If exit at right of this room
+        room_map[middle_row][room_width - 1] = floor_type
+        room_map[middle_row+1][room_width - 1] = floor_type
+        room_map[middle_row-1][room_width - 1] = floor_type
+    
+    if current_room % MAP_WIDTH != 1:
+        room_to_left = GAME_MAP[current_room - 1]
+        if room_to_left[4]:
+            room_map[middle_row][0] = floor_type
+            room_map[middle_row + 1][0] = floor_type
+            room_map[middle_row - 1][0] = floor_type
+
+    if room_data[3]: # if room has exit at top
+        room_map[0][middle_column] = floor_type
+        room_map[0][middle_column + 1] = floor_type
+        room_map[0][middle_column - 1] = floor_type
+
+    if current_room <= MAP_SIZE - MAP_WIDTH: #If room is not on bottom row
+        room_below = GAME_MAP[current_room+MAP_WIDTH]
+        # If room below has top exit, add bottom exit to this room
+        if room_below[3]:
+            room_map[room_height-1][middle_column] = floor_type
+            room_map[room_height-1][middle_column + 1] = floor_type
+            room_map[room_height-1][middle_column - 1] = floor_type
+            
